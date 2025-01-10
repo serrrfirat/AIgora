9b385116e5416b6ac623b7111f303fa044d3a7d6
Pablo Lozano <nivolabs@proton.me>
2025-01-10 12:33:39 +0100

    WIP
    - missing twitter scrapper
    - cannot construct memory in openai model

--- debate-agent/src/utils/generator.ts
+++ debate-agent/src/utils/generator.ts
@@ -1,3 +1,4 @@
+import { ModelProviderName } from '@elizaos/core';
 import { convertToOneLine, replaceAgentValues, lc, createOrUpdateJsonFile, AgentData, defaultTwitterTemplate } from './helpers';
 import { systemPrompt } from './prompts';
 
@@ -12,14 +13,46 @@ export function generateOrUpdateGladiatorData(agentName: string, agentRole: stri
 	const replacedSystemPrompt = replaceAgentValues(systemPrompt, agentName, agentRole, agentPersonality);
 	const systemPromptOneLine = convertToOneLine(replacedSystemPrompt);
 
-	const newData: AgentData = {
+	const templateData: AgentData = {
 		name: agentName,
 		system: systemPromptOneLine,
 		template: defaultTwitterTemplate,
 	}
 
+	const fullAgent = {
+		...templateData,
+		modelProvider: ModelProviderName.OPENAI,
+		bio: [agentRole, agentPersonality],
+		lore: ["previous gladiator fighting in the Colisseum turned debate-champion from their city"],
+		messageExamples: [
+			[
+				{
+					"user": "{{user1}}",
+					"content": {
+						"text": "What's your stance on abortion?"
+					}
+				},
+				{
+					"user": "Spartacus",
+					"content": {
+						"text": "Everyone should have the right to abort."
+					}
+				}
+			],
+		],
+		postExamples: [
+			"Read a book, you stupid fool",
+		],
+		topics: ["politics", "democracy", ""],
+		adjectives: ["passionate", "creative"],
+		clients: ["direct"],
+		plugins: [],
+		style: { all: ["informal"], chat: ["lengthy"], post: ["concise"] },
+		settings: { model: "gpt-o1-mini" },
+	}
+
 	const filePath = `./characters/${lc(agentName)}.character.json`;
 
-	createOrUpdateJsonFile(filePath, newData);
+	createOrUpdateJsonFile(filePath, fullAgent);
 }
 
