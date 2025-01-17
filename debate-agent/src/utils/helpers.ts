6646dd1d6640aa615a7e8c409de6c3d6a2240769
Pablo Lozano <nivolabs@proton.me>
2025-01-17 18:08:57 +0100

    feat: create judge and feed memories to an agent
    Removes previous files in the coordinator (courts) to use the judge
    there

--- debate-agent/src/utils/helpers.ts
+++ debate-agent/src/utils/helpers.ts
@@ -1,46 +1,48 @@
 import fs from 'fs';
 import { continueMessageHandlerTemplate, evaluationTemplate, factsTemplate, goalsTemplate, shouldRespondTemplate, twitterActionTemplate, twitterMessageHandlerTemplate, twitterPostTemplate, twitterSearchTemplate, twitterShouldRespondTemplate } from './templates.ts';
 import { messageHandlerTemplate } from '@elizaos/client-direct';
+import { AgentRuntime, Memory, UUID } from '@elizaos/core';
+import { parse as parseUuid, stringify, v4 as uuid } from "uuid";
 
 /**
  * Template interface for what's needed for the Twitter agent.
  */
 export interface TwitterTemplate {
-	goalsTemplate: string,
-	factsTemplate: string,
-	messageHandlerTemplate: string,
-	shouldRespondTemplate: string,
-	continueMessageHandlerTemplate: string,
-	evaluationTemplate: string,
-	twitterSearchTemplate: string,
-	twitterPostTemplate: string,
-	twitterActionTemplate: string,
-	twitterMessageHandlerTemplate: string,
-	twitterShouldRespondTemplate: string,
+  goalsTemplate: string,
+  factsTemplate: string,
+  messageHandlerTemplate: string,
+  shouldRespondTemplate: string,
+  continueMessageHandlerTemplate: string,
+  evaluationTemplate: string,
+  twitterSearchTemplate: string,
+  twitterPostTemplate: string,
+  twitterActionTemplate: string,
+  twitterMessageHandlerTemplate: string,
+  twitterShouldRespondTemplate: string,
 }
 
 export const defaultTwitterTemplate: TwitterTemplate = {
-	goalsTemplate: convertToOneLine(goalsTemplate),
-	factsTemplate: convertToOneLine(factsTemplate),
-	messageHandlerTemplate: convertToOneLine(messageHandlerTemplate),
-	shouldRespondTemplate: convertToOneLine(shouldRespondTemplate),
-	continueMessageHandlerTemplate: convertToOneLine(continueMessageHandlerTemplate),
-	evaluationTemplate: convertToOneLine(evaluationTemplate),
-	twitterSearchTemplate: convertToOneLine(twitterSearchTemplate),
-	twitterPostTemplate: convertToOneLine(twitterPostTemplate),
-	twitterActionTemplate: convertToOneLine(twitterActionTemplate),
-	twitterMessageHandlerTemplate: convertToOneLine(twitterMessageHandlerTemplate),
-	// FIXME: correct handlers + MarcusAIurelius
-	twitterShouldRespondTemplate: convertToOneLine(twitterShouldRespondTemplate("@Gladiator1,@Gladiator2,@Gladiator3")),
+  goalsTemplate: convertToOneLine(goalsTemplate),
+  factsTemplate: convertToOneLine(factsTemplate),
+  messageHandlerTemplate: convertToOneLine(messageHandlerTemplate),
+  shouldRespondTemplate: convertToOneLine(shouldRespondTemplate),
+  continueMessageHandlerTemplate: convertToOneLine(continueMessageHandlerTemplate),
+  evaluationTemplate: convertToOneLine(evaluationTemplate),
+  twitterSearchTemplate: convertToOneLine(twitterSearchTemplate),
+  twitterPostTemplate: convertToOneLine(twitterPostTemplate),
+  twitterActionTemplate: convertToOneLine(twitterActionTemplate),
+  twitterMessageHandlerTemplate: convertToOneLine(twitterMessageHandlerTemplate),
+  // FIXME: correct handlers + MarcusAIurelius
+  twitterShouldRespondTemplate: convertToOneLine(twitterShouldRespondTemplate("@Gladiator1,@Gladiator2,@Gladiator3")),
 };
 
 /**
  * JSON object storing the data for the agent. Basically here so the compiler doesn't yell.
  */
 export interface AgentData {
-	name: string,
-	system: string,
-	template: TwitterTemplate,
+  name: string,
+  system: string,
+  template: TwitterTemplate,
 }
 
 /**
@@ -48,11 +50,11 @@ export interface AgentData {
  * @returns a string without linebreaks
  */
 export function convertToOneLine(text: string): string {
-	return text
-		.replace(/\r\n|\r|\n/g, '\\n')
-		.replace(/"/g, '\\"')
-		.replace(/\s+/g, ' ')
-		.trim();
+  return text
+    .replace(/\r\n|\r|\n/g, '\\n')
+    .replace(/"/g, '\\"')
+    .replace(/\s+/g, ' ')
+    .trim();
 }
 
 /**
@@ -63,10 +65,10 @@ export function convertToOneLine(text: string): string {
  * @returns same multiline text without placeholders
  */
 export function replaceAgentValues(text: string, agentName: string, agentRole: string, agentPersonality: string): string {
-	return text
-		.replace(/{{AGENT_NAME}}/g, agentName)
-		.replace(/{{AGENT_ROLE}}/g, agentRole)
-		.replace(/{{AGENT_PERSONALITY}}/g, agentPersonality);
+  return text
+    .replace(/{{AGENT_NAME}}/g, agentName)
+    .replace(/{{AGENT_ROLE}}/g, agentRole)
+    .replace(/{{AGENT_PERSONALITY}}/g, agentPersonality);
 }
 
 /**
@@ -74,7 +76,7 @@ export function replaceAgentValues(text: string, agentName: string, agentRole: s
  * @returns lowercased string
  */
 export function lc(str: string): string {
-	return str.toLowerCase();
+  return str.toLowerCase();
 }
 
 
@@ -84,17 +86,17 @@ export function lc(str: string): string {
  * @param filePath - the path to the JSON file
  */
 export function loadCharacter(filePath: fs.PathLike): AgentData {
-	let existingData = {} as AgentData;
+  let existingData = {} as AgentData;
 
-	if (fs.existsSync(filePath)) {
-		const fileContent = fs.readFileSync(filePath, 'utf-8');
-		existingData = JSON.parse(fileContent);
-		console.log("Existing character found. Updating...");
-	} else {
-		console.log("No existing character found...");
-	}
+  if (fs.existsSync(filePath)) {
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    existingData = JSON.parse(fileContent);
+    console.log("Existing character found. Updating...");
+  } else {
+    console.log("No existing character found...");
+  }
 
-	return existingData;
+  return existingData;
 }
 
 /** Create or update a JSON object
@@ -103,29 +105,29 @@ export function loadCharacter(filePath: fs.PathLike): AgentData {
  * @param newData - the new JSON object
  */
 export function createOrUpdateJsonFile(filePath: fs.PathLike, newData: AgentData) {
-	let existingData = loadCharacter(filePath);
+  let existingData = loadCharacter(filePath);
 
-	// Merge existing data with new data
-	const updatedData = {
-		...existingData,
-		...newData,
-		template: {
-			...existingData.template,
-			...newData.template
-		}
-	};
+  // Merge existing data with new data
+  const updatedData = {
+    ...existingData,
+    ...newData,
+    template: {
+      ...existingData.template,
+      ...newData.template
+    }
+  };
 
-	// Convert JSON object to string
-	const jsonString = JSON.stringify(updatedData, null, 2);
+  // Convert JSON object to string
+  const jsonString = JSON.stringify(updatedData, null, 2);
 
-	// Write to file
-	if (!fs.existsSync("characters/")) {
-		console.log("Creating characters directory...");
-		fs.mkdirSync("characters/");
-	}
-	fs.writeFileSync(filePath, jsonString);
+  // Write to file
+  if (!fs.existsSync("characters/")) {
+    console.log("Creating characters directory...");
+    fs.mkdirSync("characters/");
+  }
+  fs.writeFileSync(filePath, jsonString);
 
-	console.log(`JSON file '${filePath}' has been ${fs.existsSync(filePath) ? 'updated' : 'created'} successfully.`);
+  console.log(`JSON file '${filePath}' has been ${fs.existsSync(filePath) ? 'updated' : 'created'} successfully.`);
 }
 
 
@@ -133,6 +135,39 @@ export function createOrUpdateJsonFile(filePath: fs.PathLike, newData: AgentData
  * Combines two objects into a new one, keepeing fields from both.
  */
 export function combineObjects<T extends object, U extends object>(obj1: T, obj2: U): T & U {
-	return { ...obj1, ...obj2 };
+  return { ...obj1, ...obj2 };
 }
 
+
+/**
+ * The least information need to feed memories to an agent.
+ *
+ * Observe that the `sender` field is different from the 
+ * field in the `debate-coordinator`. It uses the UUID from eliza,
+ * since the memory manager uses it too.
+ */
+interface Message {
+  sender: UUID;
+  content: string;
+}
+
+/**
+ * Feeds messages as memories to an already existing agent.
+ * Usually used for the judge to get the contents on the debate in memory.
+ *
+ * @param judge: the agent to use RAG with.
+ * @param roomId: the room UUID (as defined in eliza) of the debate.
+ * @param messages: the messages that were sent in the room.
+ */
+export function createMemories(judge: AgentRuntime, roomId: UUID, messages: Message[]) {
+  const memoryManager = judge.getMemoryManager("memory");
+  for (const message of messages) {
+    const memory: Memory = {
+      userId: "0-0-0-0-0",
+      agentId: message.sender,
+      content: { text: message.content },
+      roomId: roomId,
+    }
+    memoryManager.createMemory(memory)
+  }
+}
